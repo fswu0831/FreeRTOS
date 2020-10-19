2206363bf35d91e1c28a3c085af97c2b70676a5e
fswu0831 <60534708+fswu0831@users.noreply.github.com>
2020-10-19 20:37:37 +0900

    add mutex

--- Demo/WIN32-MSVC/main_blinky.c
+++ Demo/WIN32-MSVC/main_blinky.c
@@ -1,87 +1,3 @@
-/*
- * FreeRTOS Kernel V10.4.1
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
- *
- * 1 tab == 4 spaces!
- */
-
-/******************************************************************************
- * NOTE: Windows will not be running the FreeRTOS demo threads continuously, so
- * do not expect to get real time behaviour from the FreeRTOS Windows port, or
- * this demo application.  Also, the timing information in the FreeRTOS+Trace
- * logs have no meaningful units.  See the documentation page for the Windows
- * port for further information:
- * http://www.freertos.org/FreeRTOS-Windows-Simulator-Emulator-for-Visual-Studio-and-Eclipse-MingW.html
- *
- * NOTE 2:  This project provides two demo applications.  A simple blinky style
- * project, and a more comprehensive test and demo application.  The
- * mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting in main.c is used to select
- * between the two.  See the notes on using mainCREATE_SIMPLE_BLINKY_DEMO_ONLY
- * in main.c.  This file implements the simply blinky version.  Console output
- * is used in place of the normal LED toggling.
- *
- * NOTE 3:  This file only contains the source code that is specific to the
- * basic demo.  Generic functions, such FreeRTOS hook functions, are defined
- * in main.c.
- ******************************************************************************
- *
- * main_blinky() creates one queue, one software timer, and two tasks.  It then
- * starts the scheduler.
- *
- * The Queue Send Task:
- * The queue send task is implemented by the prvQueueSendTask() function in
- * this file.  It uses vTaskDelayUntil() to create a periodic task that sends
- * the value 100 to the queue every 200 milliseconds (please read the notes
- * above regarding the accuracy of timing under Windows).
- *
- * The Queue Send Software Timer:
- * The timer is a one-shot timer that is reset by a key press.  The timer's
- * period is set to two seconds - if the timer expires then its callback
- * function writes the value 200 to the queue.  The callback function is
- * implemented by prvQueueSendTimerCallback() within this file.
- *
- * The Queue Receive Task:
- * The queue receive task is implemented by the prvQueueReceiveTask() function
- * in this file.  prvQueueReceiveTask() waits for data to arrive on the queue.
- * When data is received, the task checks the value of the data, then outputs a
- * message to indicate if the data came from the queue send task or the queue
- * send software timer.
- *
- * Expected Behaviour:
- * - The queue send task writes to the queue every 200ms, so every 200ms the
- *   queue receive task will output a message indicating that data was received
- *   on the queue from the queue send task.
- * - The queue send software timer has a period of two seconds, and is reset
- *   each time a key is pressed.  So if two seconds expire without a key being
- *   pressed then the queue receive task will output a message indicating that
- *   data was received on the queue from the queue send software timer.
- *
- * NOTE:  Console input and output relies on Windows system calls, which can
- * interfere with the execution of the FreeRTOS Windows port.  This demo only
- * uses Windows system call occasionally.  Heavier use of Windows system calls
- * can crash the port.
- */
-
 /* Standard includes. */
 #include <stdio.h>
 #include <conio.h>
@@ -124,6 +40,7 @@ static void consumer( void *pvParameters );
 /*-----------------------------------------------------------*/
 
 static  UBaseType_t N=100;
+static  UBaseType_t zero = 0;
  
 /* The queue used by both tasks. */
 static QueueHandle_t xQueue = NULL;
@@ -155,7 +72,7 @@ void main_blinky( void )
 
 	//creating resouses
 	sem_empty = xSemaphoreCreateCounting(N, N);
-	sem_full = xSemaphoreCreateCounting(0, N);
+	sem_full = xSemaphoreCreateCounting(N,0);
 	mutex = xSemaphoreCreateMutex();
 
 
@@ -176,7 +93,7 @@ static void producer( void *pvParameters )
 				if (mutex != NULL) {
 					if (xSemaphoreTake(mutex, (TickType_t)10) == pdTRUE){
 						//insert item
-						printf("producer");
+						printf("producer %lf\n",sem_empty);
 						xSemaphoreGive(mutex);
 					}
 
@@ -186,7 +103,7 @@ static void producer( void *pvParameters )
 			
 		}
 		else {
-			printf("don get semaphore\n");
+			printf("don't get semaphore\n");
 		}
 		
 		vTaskDelay(100);
@@ -200,16 +117,28 @@ static void producer( void *pvParameters )
 
 static void consumer( void *pvParameters )
 {
-
-
 	( void ) pvParameters;
 
 	while (1)
 	{
 
-		printf("task2");
-		/* Reset the software timer. */
-		vTaskDelay(100);
+		if (sem_full != NULL) {
+			if (xSemaphoreTake(sem_full, (TickType_t)10) == pdTRUE) {
+				if (mutex != NULL) {
+					if (xSemaphoreTake(mutex, (TickType_t)10) == pdTRUE) {
+						//remove item
+						printf("consumer\n");
+						xSemaphoreGive(mutex);
+					}
+
+				}
+				xSemaphoreGive(sem_full);
+			}
+
+		}
+		else {
+			printf("don't get semaphore\n");
+		}
 	
 	}
 }
