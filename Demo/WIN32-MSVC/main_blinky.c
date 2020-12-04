77dbada053f83a069846dd2f889ab1288a631183
fswu0831 <60534708+fswu0831@users.noreply.github.com>
2020-12-04 22:43:35 +0900

    message

--- Demo/WIN32-MSVC/main_blinky.c
+++ Demo/WIN32-MSVC/main_blinky.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 
 /* Kernel includes. */
 #include "FreeRTOS.h"
@@ -9,6 +10,7 @@
 #include "timers.h"
 #include "semphr.h"
 #include "queue.h"
+#include "message_buffer.h"
 
 #define LOG_FILE     "LOG/log.txt"  
 FILE* log_file;
@@ -30,94 +32,51 @@ queue send software timer respectively. */
 #define mainVALUE_SENT_FROM_TASK			( 100UL )
 #define mainVALUE_SENT_FROM_TIMER			( 200UL )
 
-/*-----------------------------------------------------------*/
+
+#define SEND_TASK_NUM (1)  //送信タスクの数
+#define RECEIVE_TASK_NUM (1)  //受信タスクの数
+
 
 /*
  * The tasks as described in the comments at the top of this file.
  */
-static void producer( void *pvParameters );
-static void consumer( void *pvParameters );
-//void array_push(void* ptr, int size, size_t unit_size, void* item);
-//static void consumer2(void* pvParameters);
-
-/*
- * The callback function executed when the software timer expires.
- */
-
-/*-----------------------------------------------------------*/
+static void SEND_TASK( void *pvParameters );
+static void RECEIVE_TASK( void *pvParameters );
 
 static UBaseType_t N=5;
 
 
- 
-/* The queue used by both tasks. */
-static QueueHandle_t myQueue;
+
 
 /* A software timer that is started from the tick hook. */
 static TimerHandle_t xTimer = NULL;
 
-static SemaphoreHandle_t sem_empty;
-static SemaphoreHandle_t sem_full;
-static SemaphoreHandle_t mutex;
+//static MessageBufferHandle_t xMessageBuffer;
+static MessageBufferHandle_t xMessageBuffer;
+static const size_t xMessageBufferSizeBytes = 100;
 
-static int empty = 5; //3
-static int full = 0; //2
-static int mu = 0;
+static int i = 1;//ループ用の関数
+static int j = 1;
 
-
-static int buffer = 0; //資源の初期値
-
-static int work_num=100; //消費者のループ回数
-//static int work_num_producer = 100; //生産者のループ回数
-
-static int count=1;
-static int i = 0;//CSV保存のための変数
-
-static char myTxBuff[5] ;
-static char myRxBuff[5] ;
-
-
-/*---------CSVに保存するための関数など------------*/
-/*
-typedef struct {
-	int x, y;
-}Vector;
-
-static Vector* ary = (Vector*)malloc(sizeof(Vector) * 100);
-
-static void save_csv(void) {
-	FILE* fp;
-	if ((fp = fopen("data.csv", "w")) != NULL) {
-		for (i; i < work_num; i++) {
-			fprintf(fp, "%d,%d\n", ary[i].x, ary[i].y);
-		}
-		fclose(fp);
-	}
-	return 0;
-}
-
-
-*/
-/*---------CSVに保存するための関数など------------*/
+static char T[10];
 
 
 static void create_object(void) {
 	/* Start the two tasks as described in the comments at the top of this
 	file. */
-	xTaskCreate(producer,			/* The function that implements the task. */
-		"pro1", 							/* The text name assigned to the task - for debug only as it is not used by the kernel. */
-		configMINIMAL_STACK_SIZE, 		/* The size of the stack to allocate to the task. */
-		NULL, 							/* The parameter passed to the task - not used in this simple case. */
-		main_TASK_PRIORITY_HIGH,/* The priority assigned to the task. */
-		NULL);							/* The task handle is not required, so NULL is passed. */
 
-	xTaskCreate(consumer, "con1", configMINIMAL_STACK_SIZE, NULL, main_TASK_PRIORITY_LOW, NULL);
-	//xTaskCreate(consumer2, "con2", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL);
+	for (i; i <= SEND_TASK_NUM; i++) {
+		sprintf(T, "T%d", i);
+		xTaskCreate(RECEIVE_TASK, T , configMINIMAL_STACK_SIZE, NULL, main_TASK_PRIORITY_HIGH, NULL);
+		
+	}
 
-	//creating resouses
-	sem_empty = xSemaphoreCreateCounting(N, N); //N
-	sem_full = xSemaphoreCreateCounting(N, 0);  //0
-	mutex = xSemaphoreCreateMutex();
+	for (j; j <= RECEIVE_TASK_NUM; j++) {
+		sprintf(T, "T%d", j+SEND_TASK_NUM);
+		xTaskCreate(SEND_TASK, T , configMINIMAL_STACK_SIZE, NULL, main_TASK_PRIORITY_HIGH, NULL);
+	}
+
+	xMessageBuffer = xMessageBufferCreate(xMessageBufferSizeBytes);
 }
 
 
@@ -129,97 +88,59 @@ void main_blinky( void )
 }
 /*-----------------------------------------------------------*/
 
+/*メッセージバッファのための変数*/
+
+uint8_t ucArrayToSend[] = { 0,1,2,3 };
+size_t xBytesSent;
+static const TickType_t x100ms = pdMS_TO_TICKS(100);
 
 
-static void producer( void *pvParameters )
+uint8_t ucRxData[20];
+size_t xReceivedBytes;
+const TickType_t xBlockTime = pdMS_TO_TICKS(20);
+
+
+static void SEND_TASK( void *pvParameters )
 {
 	( void ) pvParameters;
-	myQueue = xQueueCreate(N, sizeof(myTxBuff));
-	//xQueCreate(5,sizeof())//
 
-	for(count; count <=work_num; count++)
+	while(1)
 	{
+		xBytesSent = xMessageBufferSend(xMessageBuffer, (void *)ucArrayToSend, sizeof(ucArrayToSend), x100ms);
 
-		if (sem_empty != NULL) {
-			if (xSemaphoreTake(sem_empty, (TickType_t)0xFFFFFFFF) == pdTRUE) {
-				empty--;
-				if (mutex != NULL) {
-					if (xSemaphoreTake(mutex, (TickType_t)0xFFFFFFFF) == pdTRUE){
-						//insert item
-						sprintf(myTxBuff, "item");
-						xQueueSend(myQueue, (void*)myTxBuff, (TickType_t)0);
-						printf("==================================\n");
-						printf("producer:%d\n", count);
-						printf("waiting to be read:%d\n", uxQueueMessagesWaiting(myQueue));
-						printf("available spases  :%d\n", uxQueueSpacesAvailable(myQueue));
-						printf("==================================\n\n");
-						xSemaphoreGive(mutex);
-					}
-					else {
-						xSemaphoreGive(sem_empty);
-						//mutex取れなかった時→基本的にここにはこない
-					}
-
-				}
-				else {
-					//mutexが取られている
-				}
-				xSemaphoreGive(sem_full);
-			}
-			else {
-				//セマフォ取れなかった→基本的にここにはこない
-			}
+		if (xBytesSent != sizeof(ucArrayToSend)) {
+			//dont have enough space
+			printf("not enough send spase\n");
 		}
 		else {
-			//セマフォがない
+			printf("send:%d\n",xBytesSent);
 		}
+
 		vTaskDelay(10);
 	}
-	vTaskEndScheduler();
+	//vTaskEndScheduler();
 }
 
 
-static void consumer( void *pvParameters )
+static void RECEIVE_TASK( void *pvParameters )
 {
 	( void ) pvParameters;
-	for (count; count <= work_num; count++)
+
+
+
+	while (1)
 	{
-		
-		if (sem_full != NULL) {
-			if (xSemaphoreTake(sem_full, (TickType_t)0xFFFFFFFF) == pdTRUE) { //(TickType_t)0) == pdTRUE
-				full--;
-				if (mutex != NULL) {
-					if (xSemaphoreTake(mutex, (TickType_t)0xFFFFFFFF) == pdTRUE) {
-						xQueueReceive(myQueue, (void*)myRxBuff,(TickType_t) 5 );
-						printf("==================================\n");
-						printf("consumer:%d\n", count);
-						printf("waiting to be read:%d\n", uxQueueMessagesWaiting(myQueue));
-						printf("available spases  :%d\n", uxQueueSpacesAvailable(myQueue));
-						printf("==================================\n\n");
-						xSemaphoreGive(mutex);
-					}
-					else {
-						xSemaphoreGive(sem_full);
-						//mutex取れなかった時→基本的にここにはこない
-					}
+		xReceivedBytes = xMessageBufferReceive(xMessageBuffer, (void*)ucRxData, sizeof(ucRxData), xBlockTime);
 
-
-				}
-				else {
-					//mutexが取られている
-				}
-				xSemaphoreGive(sem_empty);
-			}
-			else {
-				//セマフォ取れなかった→基本的にここにはこない
-			}
-
+		if (xReceivedBytes>0) {
+			//dont have enough space
+			printf("rece:%d\n", ucRxData[1]);
 		}
 		else {
-			//セマフォがない
+			printf("ERROR\n");
 		}
 
 		vTaskDelay(10);
 	}
-	vTaskEndScheduler();
+	//vTaskEndScheduler();
 }
